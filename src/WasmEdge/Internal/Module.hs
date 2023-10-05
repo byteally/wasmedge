b555f2be81a3190e3e98fff3ec271969d3a650ea
Magesh <magesh85@gmail.com>
2023-10-05 17:18:52 +0530

    Fix the interface for astModuleListImports, astModuleListExports Add Internal ASTModule

---
+++ src/WasmEdge/Internal/Module.hs
@@ -0,0 +1,11 @@
+module WasmEdge.Internal.Module
+  ( ModuleDescriptor
+  , ASTModuleContext
+  , ModuleInstanceContext
+  , astModuleListImportsLength
+  , astModuleListImports
+  , astModuleListExportsLength
+  , astModuleListExports
+  ) where
+
+import WasmEdge.Internal.FFI.Bindings
--- src/WasmEdge/Module.hs
+++ src/WasmEdge/Module.hs
@@ -1,9 +1,24 @@
 module WasmEdge.Module
-  ( ModuleDescriptor
-  , ASTModuleContext
-  , ModuleInstanceContext
-  , astModuleListImportsLength
-  , astModuleListExportsLength
+  ( module WasmEdge.Internal.Module
+  , astModuleListImports
+  , astModuleListExports
+  , Vector
   ) where
 
-import WasmEdge.Internal.FFI.Bindings
+import WasmEdge.Internal.Module
+  hiding ( astModuleListImports
+         , astModuleListExports
+         )
+import qualified WasmEdge.Internal.FFI.Bindings as Int
+import Data.Vector.Storable (Vector)
+
+astModuleListImports
+  :: ASTModuleContext
+  -> IO (Vector Int.ImportTypeContext)
+astModuleListImports ast = Int.astModuleListImports ast =<< astModuleListImportsLength ast
+
+astModuleListExports
+  :: ASTModuleContext
+  -> IO (Vector Int.ExportTypeContext)
+astModuleListExports ast = Int.astModuleListExports ast =<< astModuleListExportsLength ast
+  
